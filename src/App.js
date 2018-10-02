c49819ba3c54fdda2024891ef579a6b8a4bbb47f
taufiq ibrahim <taufiq.ibrahim@gmail.com>
2018-10-03 00:59:20 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,27 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {StaticMap} from 'react-map-gl';
+// import MapGL from 'react-map-gl';
+import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoidGlicmFoaW0iLCJhIjoiY2ptbHJ2eTFkMGJldDNxcGt4cXk5MjE0cCJ9.ax5_fH9jKStEWjH_qf_SLQ';
+// var data_sites = require('./data/sites_scatter.json');
+// console.log(data_sites);
+const data_sites = []
+
+export const INITIAL_VIEW_STATE = {
+  longitude: 106.8173201,
+  latitude: -6.2307753,
+  zoom: 11,
+  maxZoom: 16,
+  pitch: 45,
+  bearing: 0
+}
 
 class App extends Component {
+
+  _renderLayers() {
+    const {
+      radius = 1000,
+    } = this.props;
+
+    return [
+      new ScatterplotLayer({
+        id: 'sites',
+        data: data_sites,
+        radiusScale: radius,
+        getPosition: d => [d[3], d[4], 0],
+        getRadius: d => d[6],
+        getColor: d => [255, 140, 0, 80],
+        pickable: true
+        // onHover: d => console.log('Hovered:', d),
+        // updateTriggers: {
+        //   getColor: [maleColor, femaleColor]
+        // }
+      })
+    ];
+  }
+
   render() {
+
+    const {viewState, controller = true, baseMap = true} = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        // viewState={viewState}
+        // controller={controller}
+        layers={this._renderLayers()}
+      >
+        
+          <StaticMap
+            mapboxApiAccessToken={MAPBOX_TOKEN}
+          />
+        
+      </DeckGL>
+    )
   }
 }
 
